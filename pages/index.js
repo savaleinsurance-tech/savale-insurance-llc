cc651ecb5d1600386a80df1e2a03fa372c10a1bf
savaleinsurance-tech <savaleinsurance@gmail.com>
2025-08-24 18:23:44 -0400

    Create index.js
    export default function Home() {
      const btn = (bg) => ({
        background: bg,
        color: "#fff",
        borderRadius: 14,
        padding: "10px 16px",
        textDecoration: "none",
        display: "inline-block"

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  const btn = (bg) => ({
+    background: bg,
+    color: "#fff",
+    borderRadius: 14,
+    padding: "10px 16px",
+    textDecoration: "none",
+    display: "inline-block"
